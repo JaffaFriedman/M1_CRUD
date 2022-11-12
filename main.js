ba348aa9ac288120c22fe78edd9f32fae98248e6
JaffaFriedman <jafriedman.dwfsc4@bootcampudd.cl>
2022-11-12 18:28:18 -0300

    Formatos
    Actualizacion de formatos

--- main.js
+++ main.js
@@ -5,23 +5,23 @@ Jaffa Friedman
 let tareas=[]
 
 function agregarTarea(tarea,descripcion) {
-  tarea ={ 
-    tarea: tarea,
-    descripcion: descripcion, }
-    tareas.push(tarea)
-    grabarStorage()
+    tarea ={ 
+        tarea: tarea,
+        descripcion: descripcion, }
+        tareas.push(tarea)
+        grabarStorage()
 }
 
 function  titulos() {
-  const tareaE = document.createElement("div")
-  tareaE.innerHTML = `<div class="border rounded-top bg-secondary text-bg-secondary fw-bold">
-                        <div class="container row " id="lista">
-                            <div class="col-2" id="tarea">Tarea</div>
-                            <div class="col-8" id="descripcion">Descripción</div>
-                            <div class="col-2"> </div>   
-                        </div>  
-                    </div>`
-    listar.appendChild(tareaE)
+    const tareaE = document.createElement("div")
+    tareaE.innerHTML = `<div class="border rounded-top bg-secondary text-bg-secondary fw-bold">
+                            <div class="container row " id="lista">
+                                <div class="col-2" id="tarea">Tarea</div>
+                                <div class="col-8" id="descripcion">Descripción</div>
+                                <div class="col-2"> </div>   
+                            </div>  
+                        </div>`
+        listar.appendChild(tareaE)
 }
 
 function formListar(tarea,idx) {
@@ -31,69 +31,71 @@ function formListar(tarea,idx) {
                                 <div class="col-2" id="tareaL">${tarea.tarea} </div>
                                 <div class="col-8" id="descripcionL ">${tarea.descripcion}</div>
                                 <div class="col-2">         
-                                    <button id="btn-update" type="button" class="btn btn-secondary m-1" style="width:4rem; --bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;" 
+                                    <button id="btn-update" type="button" class="btn btn-secondary m-1" style="width:4rem; --bs-btn-padding-y: .25rem; 
+                                    --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;" 
                                     onclick="formModificar(${idx})">Editar</button>
-                                    <button id="btn-delete" type="button" class="btn btn-secondary m-1" style="width:4rem;--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;" 
+                                    <button id="btn-delete" type="button" class="btn btn-secondary m-1" style="width:4rem;--bs-btn-padding-y: .25rem; 
+                                    --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;" 
                                     onclick="formEliminar(${idx})">Eliminar</button>
                                 </div>
                             </div>
                       </div>`
     listar.appendChild(tareaE)
-   }
+ }
 
    function botonAgregar() {
-    const tareaE =  document.createElement("div")
-    tareaE.innerHTML = `<div class="container">
-                            <div class="aling-end " >
-                                <button id="btn-create" type="button" class="btn btn-secondary .bg-gradient" 
-                                onclick="formIngreso()">Nueva Tarea</button>
-                            </div>
-                        </div>`
-    boton.appendChild(tareaE)
+        const tareaE =  document.createElement("div")
+        tareaE.innerHTML = `<div class="container">
+                                <div class="aling-end " >
+                                    <button id="btn-create" type="button" class="btn btn-secondary .bg-gradient" 
+                                    onclick="formIngreso()">Nueva Tarea</button>
+                                </div>
+                            </div>`
+        boton.appendChild(tareaE)
    }
 
-   function listarTareas()
-   {  borrarSeccion('listar')
-      titulos()
-      tareas.forEach((v,idx)=>formListar(v,idx))
-      botonAgregar()
+   function listarTareas() {  
+            borrarSeccion('listar')
+            titulos()
+            tareas.forEach((v,idx)=>formListar(v,idx))
+            botonAgregar()
    }
 
    function formModificar(idx ) {  
-    limpiar()
-    const tareaE = document.createElement("div")
-    tareaE.innerHTML = `<div class="card text-bg-light mb-3 fw-bold" >
-                        <div class="card-header bg-secondary text-bg-secondary">Modificar</div>
-                            <div class="card-body">
-                            <form id="formModificar">
-                                <div class="mt-2" id="tarea">
-                                    <label for="tarea" class="form-label fw-bold">Tarea</label>
-                                    <input  class="form-control" id="tareaM" value="${tareas[idx].tarea}"> </input>                               
+        limpiar()
+        const tareaE = document.createElement("div")
+        tareaE.innerHTML = `<div class="card text-bg-light mb-3 fw-bold" >
+                            <div class="card-header bg-secondary text-bg-secondary">Modificar</div>
+                                <div class="card-body">
+                                <form id="formModificar">
+                                    <div class="mt-2" id="tarea">
+                                        <label for="tarea" class="form-label fw-bold">Tarea</label>
+                                        <input  class="form-control" id="tareaM" value="${tareas[idx].tarea}"> </input>                               
+                                    </div>
+                                    <div class="mt-2" id="descripcion" >
+                                        <label for="descripcion" class="form-label fw-bold">Descripción</label>
+                                        <input  class="form-control" id="descripcionM"  rows="3" value="${tareas[idx].descripcion}"> </input>                               
+                                    </div>
+                                    <div class="aling-end mt-4 mb-4" >
+                                        <button id="btn-create" type="button" class="btn btn-outline-secondary" 
+                                        onclick="inicio()">Cancelar</button>
+                                        <button id="btn-create" type="button" class="btn btn-secondary .bg-gradient" 
+                                        onclick="modificarTarea(${idx})">Actualizar</button>
+                                    </div>
+                                </form>
                                 </div>
-                                <div class="mt-2" id="descripcion" >
-                                    <label for="descripcion" class="form-label fw-bold">Descripción</label>
-                                    <input  class="form-control" id="descripcionM"  rows="3" value="${tareas[idx].descripcion}"> </input>                               
-                                </div>
-                                <div class="aling-end mt-4 mb-4" >
-                                    <button id="btn-create" type="button" class="btn btn-outline-secondary" 
-                                    onclick="inicio()">Cancelar</button>
-                                    <button id="btn-create" type="button" class="btn btn-secondary .bg-gradient" 
-                                    onclick="modificarTarea(${idx})">Actualizar</button>
-                                </div>
-                            </form>
-                            </div>
-                    </div>`
-    modificar.appendChild(tareaE)
+                        </div>`
+        modificar.appendChild(tareaE)
    }
 
    function modificarTarea(idx){ 
-    const tarea=document.querySelector("#tareaM")
-    const descripcion=document.querySelector("#descripcionM")
-    tareas[idx].tarea=tarea.value
-    tareas[idx].descripcion=descripcion.value
-    grabarStorage()
-    limpiar() 
-    listarTareas()
+        const tarea=document.querySelector("#tareaM")
+        const descripcion=document.querySelector("#descripcionM")
+        tareas[idx].tarea=tarea.value
+        tareas[idx].descripcion=descripcion.value
+        grabarStorage()
+        limpiar() 
+        listarTareas()
    }
 
    function formEliminar(idx) { 
@@ -158,28 +160,25 @@ function formListar(tarea,idx) {
    }
  
    function ingresarTarea(){  
-    const tarea=document.querySelector("#tareaI")
-    const descripcion=document.querySelector("#descripcionI")
-    agregarTarea(tarea.value, descripcion.value);
-    const tareasJ = JSON.stringify(tareas);
-    localStorage.setItem('tareas', tareasJ);
-    inicio()
+        const tarea=document.querySelector("#tareaI")
+        const descripcion=document.querySelector("#descripcionI")
+        agregarTarea(tarea.value, descripcion.value);
+        const tareasJ = JSON.stringify(tareas);
+        localStorage.setItem('tareas', tareasJ);
+        inicio()
    } 
 
    function borrarSeccion(seccion){
- 
-    const borrar=document.getElementById(seccion) 
-    console.log(borrar,seccion,'antes')
-    borrar.innerHTML = "" 
-    console.log(borrar,seccion,'despues')
+        const borrar=document.getElementById(seccion) 
+        borrar.innerHTML = "" 
    }
 
    function limpiar() {
-    borrarSeccion('listar')
-    borrarSeccion('boton')
-    borrarSeccion('eliminar')
-    borrarSeccion('modificar')
-    borrarSeccion('ingresar')
+        borrarSeccion('listar')
+        borrarSeccion('boton')
+        borrarSeccion('eliminar')
+        borrarSeccion('modificar')
+        borrarSeccion('ingresar')
    }
 
    function leerStorage()
@@ -189,20 +188,20 @@ function formListar(tarea,idx) {
 
    function grabarStorage()
    {
-    const tareasJ = JSON.stringify(tareas);
-    localStorage.setItem('tareas', tareasJ);
+        const tareasJ = JSON.stringify(tareas);
+        localStorage.setItem('tareas', tareasJ);
    }
    
    function inicio()  {
-   leerStorage()
-   const cantidadTareas=tareas.length
-   limpiar()
-   if (cantidadTareas===0) 
-     formIngreso()
-   else
-     listarTareas()
+        leerStorage()
+        const cantidadTareas=tareas.length
+        limpiar()
+        if (cantidadTareas===0) 
+            formIngreso()
+        else
+            listarTareas()
    }
 
    window.onload = function() { 
-    inicio() 
+        inicio() 
    }
